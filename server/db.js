663edf84a12646ce69c1121a39f1226c81679987
William <williwiser@gmail.com>
2024-05-13 19:24:29 +0200

    readd ssl

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const db = new pg.Client({
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  //ssl: "require",
+  ssl: true,
 });
 
 export default db;
