b1ddd1ba3ff7883cd7a2877e1651b920976eacb6
William <williwiser@gmail.com>
2024-06-05 11:12:38 +0200

    fix active link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../header.css";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 function Header() {
   const [toggle, toggleNav] = useState(true);
+  const [activeSection, setActiveSection] = useState("hero-sn");
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const options = {
+      root: null,
+      threshold: 0.8, // Adjust as needed
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    }, options);
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
 
   function handleClick() {
     toggleNav(!toggle);
@@ -28,27 +55,44 @@ function Header() {
           <nav className={`nav ${toggle ? "show-menu" : ""}`}>
             <ul>
               <li>
-                <a href="#hero-sn" className="lk">
+                <a
+                  href="#hero-sn"
+                  className={`lk ${
+                    activeSection === "hero-sn" ? "active" : ""
+                  }`}
+                >
                   Home
                 </a>
               </li>
               <li>
-                <a href="#sn-2" className="lk">
+                <a
+                  href="#sn-2"
+                  className={`lk ${activeSection === "sn-2" ? "active" : ""}`}
+                >
                   About
                 </a>
               </li>
               <li>
-                <a href="#sn-3" className="lk">
+                <a
+                  href="#sn-3"
+                  className={`lk ${activeSection === "sn-3" ? "active" : ""}`}
+                >
                   Skills
                 </a>
               </li>
               <li>
-                <a href="#sn-4" className="lk">
+                <a
+                  href="#sn-4"
+                  className={`lk ${activeSection === "sn-4" ? "active" : ""}`}
+                >
                   Projects
                 </a>
               </li>
               <li>
-                <a href="#sn-5" className="lk">
+                <a
+                  href="#sn-5"
+                  className={`lk ${activeSection === "sn-5" ? "active" : ""}`}
+                >
                   Contact
                 </a>
               </li>
